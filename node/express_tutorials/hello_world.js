7eb147b51e0395fc7f7988db1724b33e4dd9ce85
Leroy Kang <acain589@gmail.com>
2025-05-03 18:59:25 -0400

    learning express ch1

---
+++ node/express_tutorials/hello_world.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res) {
+	res.send("Hello, world!");
+});
+
+app.listen(3000, function() {
+	console.log("Express app started on port 3000.");
+});
+
