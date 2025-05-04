8d06fdc2cd8dc6819523e0575be5a492cbfc66fa
Leroy Kang <acain589@gmail.com>
2025-05-04 00:06:42 -0400

    Express middleware

---
+++ node/express_tutorials/ch1/myserver.js
@@ -0,0 +1,14 @@
+var http = require("http");
+function requestHandler(request, response) {
+console.log("In comes a request to: " + request.url);
+if (request.url === "/") {
+    response.end("Hello, world!");
+} else if (request.url === "/about") {
+    response.end("Welcome to the about page!")
+} else {
+    response.end("Error! File not found.")
+}
+}
+var server = http.createServer(requestHandler);
+console.log(`Now running on port 3000`)
+server.listen(3000);
