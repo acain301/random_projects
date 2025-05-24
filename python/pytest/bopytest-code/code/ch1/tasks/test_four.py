ba15e586cadee8020803928badd827e57e97a3d5
Leroy Kang <acain589@gmail.com>
2025-05-23 21:33:02 -0400

    Remove tracked __pycache__ files and update .gitignore

--- python/pytest/bopytest-code/code/ch1/tasks/test_four.py
+++ python/pytest/bopytest-code/code/ch1/tasks/test_four.py
@@ -1,7 +1,6 @@
 """Test the Task data type."""
 
 from collections import namedtuple
-import pytest
 
 Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
 Task.__new__.__defaults__ = (None, None, False, None)
@@ -11,11 +10,12 @@ def test_asdict():
     t_task = Task('do something', 'okken', True, 21)
     t_dict = t_task._asdict()
     expected = {'summary': 'do something',
-    'owner': 'okken',
-    'done': True,
-    'id': 21}
+                'owner': 'okken',
+                'done': True,
+                'id': 21}
     assert t_dict == expected
-@pytest.mark.run_these_please
+
+
 def test_replace():
     """replace() should change passed in fields."""
     t_before = Task('finish book', 'brian', False)
